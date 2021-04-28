3011ded0a3450ed6154ddc003c60061d7e17c57c
Chonwai <chonwaiun@gmail.com>
2021-04-28 19:13:39 +0800

    Add: Added some APIs.

--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,32 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::prefix('shopify')->group(function () {
+    Route::prefix('products')->group(function () {
+        Route::post('/new', function (Request $request) {
+            echo (json_encode($request));
+        });
 
+        Route::post('/update', function (Request $request) {
+            echo (json_encode($request));
+        });
+    });
+
+    Route::prefix('orders')->group(function () {
+        Route::post('/new', function (Request $request) {
+            echo (json_encode($request));
+        });
+    });
+
+    Route::prefix('users')->group(function () {
+        Route::post('/new', function (Request $request) {
+            echo (json_encode($request));
+        });
+    });
+});
